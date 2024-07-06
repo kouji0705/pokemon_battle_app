e2403f8526bbf42f30213c127362ddce729954f2
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-07-06 15:40:51 +0900

    add:

---
+++ lib/domain/entities/stats.dart
@@ -0,0 +1,13 @@
+class Stats {
+  final int hp;
+  final int attack;
+  final int defense;
+  final int speed;
+
+  Stats({
+    required this.hp,
+    required this.attack,
+    required this.defense,
+    required this.speed,
+  });
+}
